55e8c0db09b27a360b9b961f3c95cce18c298612
hoanphungt <hoanphung.cpo@gmail.com>
2019-12-24 11:29:48 +0100

    Use multiple useState() in function component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function App() {
   //new state variable
   const [count, setCount] = useState(0);
+  const [age, setAge] = useState(32);
 
   return (
     <div>
@@ -10,6 +11,23 @@ function App() {
       <button onClick={() => setCount(count + 1)}>
         Click
       </button>
+      <hr/>
+      {age === 32 ? (
+        <div>
+          <p>I am now {age} years old</p>
+          <button onClick={() => setAge(33)}>
+            My new age
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>I will be {age} years old on 29/12/2019</p>
+          <button onClick={() => setAge(32)}>
+            My current age
+          </button>
+        </div>
+      )
+      }
     </div>
   );
 }
