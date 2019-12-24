4b397b82c4bbd75e2e0049a4afc0440fbcbef9bb
hoanphungt <hoanphung.cpo@gmail.com>
2019-12-24 12:29:19 +0100

    Add useState() for age and setAge

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from 'react';
 function App() {
   //new state variable
   const [count, setCount] = useState(0);
+  const [age, setAge] = useState(32);
 
   //useEffect to perform side effects
   useEffect(() => {
     //Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  }, [count]);
+    document.title = `I am ${age}`;
+  }, [age]);
 
   return (
     <div>
@@ -16,6 +17,22 @@ function App() {
       <button onClick={() => setCount(count + 1)}>
         Click
       </button>
+      <hr/>
+      {age === 32 ? (
+        <div>
+          <p>I am now {age} years old</p>
+          <button onClick={() => setAge(33)}>
+            My new age
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>I will be {age} years old on 29/12/2019</p>
+          <button onClick={() => setAge(32)}>
+            My current age
+          </button>
+        </div>
+      )}
     </div>
   );
 }
